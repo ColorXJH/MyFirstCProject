67317a1ac964697778f884cf9fdc72b683078378
ColorXJH <1204332316@qq.com>
2022-12-14 23:37:52 +0800

    chapter01:learn C programming

--- chapter01/testMain.c
+++ chapter01/testMain.c
@@ -3,13 +3,7 @@
 //
 
 #include <stdio.h>
-
-int main(void) {
-    //macbook: option+command+l windows:ctrl+alt+l
-    // \n 换行缩紧
-    int dogs;
-    printf("How many dogs do you have?\n");
-    scanf("%d", &dogs);
-    printf("So you have %d dog(s)!\n", dogs);
+int main(void){
+    printf("console log.\n");
     return 0;
 }
